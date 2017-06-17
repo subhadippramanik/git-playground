ee4441634bf2489a5a5c4767bf6ebd25bfba6b7e
AvikManna <avik.manna95@gmail.com>
2017-06-17 13:31:20 +0530

    oaclinic part2

---
+++ oaclinic/src/test/java/com/oaclinic/OaclinicMobileAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oaclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OaclinicMobileAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
